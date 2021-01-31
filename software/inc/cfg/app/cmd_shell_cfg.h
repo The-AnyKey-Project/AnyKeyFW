0dc5c33538061d90fdd3904cc66ff0204c797487
Matthias Beckert <matthias@beckert.dev>
2021-01-31 23:09:03 +0100

    lowered priority for shell threads

--- software/inc/cfg/app/cmd_shell_cfg.h
+++ software/inc/cfg/app/cmd_shell_cfg.h
@@ -9,6 +9,6 @@
 #define INC_CFG_APP_CMD_SHELL_CFG_H_
 
 #define CMD_SHELL_WA_SIZE   2048
-#define CMD_SHELL_PRIO      (NORMALPRIO + 1)
+#define CMD_SHELL_PRIO      (NORMALPRIO - 3)
 
 #endif /* INC_CFG_APP_CMD_SHELL_CFG_H_ */
