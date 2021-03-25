f2b08435a7799d81846e94078822bb3cb6503c0d
Matthias Beckert <matthias@beckert.dev>
2021-03-25 21:28:48 +0100

    changed file headers

--- software/inc/cmd/hal/flash_storage_cmd.h
+++ software/inc/cmd/hal/flash_storage_cmd.h
@@ -1,8 +1,24 @@
 /*
+ * This file is part of The AnyKey Project  https://github.com/The-AnyKey-Project
+ *
+ * Copyright (c) 2021 Matthias Beckert
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
  * flash_storage_cmd.h
  *
  *  Created on: 04.03.2021
- *      Author: matti
+ *      Author: matthiasb85
  */
 
 #ifndef INC_CMD_HAL_FLASH_STORAGE_CMD_H_
