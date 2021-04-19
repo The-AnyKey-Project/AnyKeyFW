ade8d55fc4bce1885c6579b5cd64fa93c18dea54
Matthias Beckert <matthias@beckert.dev>
2021-04-19 21:50:40 +0200

    fixed file name

--- software/inc/cfg/hal/linker_layout_cfg.h
+++ software/inc/cfg/hal/linker_layout_cfg.h
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
- * layout.h
+ * linker_layout_cfg.h
  *
  *  Created on: 18.04.2021
  *      Author: matthiasb85
